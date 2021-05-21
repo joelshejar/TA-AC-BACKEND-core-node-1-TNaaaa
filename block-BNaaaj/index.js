277657e7c9a4624c11d7dd16d85622f2a7afc6bf
abc <joel.rajesh13@gmail.com>
2021-05-21 14:59:14 +0530

    Assignment One

---
+++ block-BNaaaj/index.js
@@ -0,0 +1,26 @@
+
+
+var os = require("os")
+os.uptime()
+os.freemem()
+os.cpus()
+os.release()
+
+
+var buff1 = Buffer.alloc(12)
+buff1.write("welcome to Node.js")
+buff1.toString()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
